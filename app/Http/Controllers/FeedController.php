09f30a1b22d4b9ed0971783f90f4a0134527bc34
Łukasz Jaroszuk <krortop@gmail.com>
2025-12-15 13:42:36 +0100

    point 6 - Pagination

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -12,7 +12,7 @@ class FeedController extends Controller
 
     const TAKE = 20;
 
-    public function get()
+    public function get(Request $req)
     {
         $friend_ids = [];
         if (auth()->check()) {
@@ -21,6 +21,12 @@ class FeedController extends Controller
                 ->pluck('friend_id')
                 ->toArray();
         }
+
+        $page = (int) $req->input('page');
+
+
+        $perPage = FeedController::TAKE;
+        $offset = ($page - 1)*$perPage;
 
         $posts = Post::when(auth()->check(), function($q) use ($friend_ids) {
                 $q->where(function($q) use ($friend_ids) {
@@ -34,7 +40,9 @@ class FeedController extends Controller
                 $q->where('status', 1);
             })
             ->with(['user', 'comments.user', 'reactions.user'])
-            ->latest()
+            ->orderBy('created_at', 'desc')
+            ->offset($offset)
+            ->limit($perPage)
             ->get()
             ->map(function($post) {
                 return [
@@ -52,10 +60,25 @@ class FeedController extends Controller
             });
 
 
-        $feed = $posts->sortByDesc('created_at')->values();
+        $feed = $posts->values();
         
+        $all = Post::when(auth()->check(), function($q) use ($friend_ids) {
+                $q->where(function($q) use ($friend_ids) {
+                    $q->where('user_id', user()->id);
+                    $q->orWhere(function($q) use ($friend_ids) {
+                        $q->whereIn('user_id', $friend_ids);
+                        $q->where('status', 1);
+                    });
+                });
+            }, function($q) {
+                $q->where('status', 1);
+            })
+            ->with(['user', 'comments.user', 'reactions.user'])->count();
+ 
+
         return response()->json([
-            'feed' => $feed
+            'feed' => $feed,
+            'totalPages' => ceil($all/$perPage)
         ], 200);
     }
 }
